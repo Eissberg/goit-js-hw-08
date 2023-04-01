0c3b4f1b46e30705f34cffaa1e247f5875fed717
Eissberg <jeka.chk@gmail.com>
2023-04-01 19:57:35 +0300

    ready 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerChangeColor;
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  timerChangeColor = setTimeout(function tick() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerChangeColor = setTimeout(tick, 1000);
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  document.body.style.backgroundColor = '#fafafa';
+  clearTimeout(timerChangeColor);
+});
